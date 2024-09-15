606d8df78d4464f167dc1293434034e54894bae5
Zuhair Malik <zuhairm2001@hotmail.com>
2024-09-15 22:53:51 +1000

    implemented single iteration gradient descent

--- src/lib.rs
+++ src/lib.rs
@@ -3,26 +3,35 @@ use std::f64;
 
 pub struct Network {
     weights: Array2<f64>,
-    hidden_weights: Array2<f64>,
     input1: Array1<f64>,
     input2: Array1<f64>,
     input3: Array1<f64>,
+    alpha: f64,
+    output_test1: Array1<f64>,
+    output_test2: Array1<f64>,
+    output_test3: Array1<f64>,
 }
 
 impl Network {
     pub fn build(
         build_weights: Array2<f64>,
-        build_hidden_weights: Array2<f64>,
         build_input1: Array1<f64>,
         build_input2: Array1<f64>,
         build_input3: Array1<f64>,
+        build_alpha: f64,
+        build_output1: Array1<f64>,
+        build_output2: Array1<f64>,
+        build_output3: Array1<f64>,
     ) -> Network {
         let network = Network {
             weights: (build_weights),
-            hidden_weights: (build_hidden_weights),
             input1: (build_input1),
             input2: (build_input2),
             input3: (build_input3),
+            alpha: (build_alpha),
+            output_test1: (build_output1),
+            output_test2: (build_output2),
+            output_test3: (build_output3),
         };
 
         network
@@ -38,7 +47,7 @@ impl Network {
         output
     }
 
-    fn vect_mat_mul(inputs_vec: Array1<f64>, weights_matrix: Array2<f64>) -> Array1<f64> {
+    fn vect_mat_mul(inputs_vec: &Array1<f64>, weights_matrix: Array2<f64>) -> Array1<f64> {
         assert_eq!(inputs_vec.len(), weights_matrix.nrows());
         let mut output: Array1<f64> = arr1(&[0.0, 0.0, 0.0]);
 
@@ -49,13 +58,53 @@ impl Network {
         output
     }
 
-    pub fn pred(&self) {
+    pub fn pred(&mut self) {
         let inputs_vec = arr1(&[self.input1[0], self.input2[0], self.input3[0]]);
-        let hidden = Self::vect_mat_mul(inputs_vec.clone(), self.hidden_weights.clone());
-        let prediction = Self::vect_mat_mul(hidden, self.weights.clone());
+        let prediction = Self::vect_mat_mul(&inputs_vec, self.weights.clone());
         println!("Prediction is ...");
-        for i in prediction {
+        for i in &prediction {
             println!("{}", i);
+        }
+        self.learn(&inputs_vec, &prediction);
+
+        let new_prediction = Self::vect_mat_mul(&inputs_vec, self.weights.clone());
+        println!("After learning ...");
+        for i in &new_prediction {
+            println!("{}", i);
+        }
+    }
+
+    fn outer_prod(vec_a: &Array1<f64>, vec_b: Array1<f64>) -> Array2<f64> {
+        let mut out: Array2<f64> = Array2::zeros((vec_a.len(), vec_b.len()));
+
+        for i in 0..vec_a.len() {
+            for j in 0..vec_b.len() {
+                out[[i, j]] = vec_a[i] * vec_b[j]
+            }
+        }
+        out
+    }
+
+    fn learn(&mut self, inputs_vec: &Array1<f64>, pred: &Array1<f64>) {
+        let mut error: Array1<f64> = Array1::zeros(3);
+        let mut delta: Array1<f64> = Array1::zeros(3);
+        let truth: Array1<f64> = arr1(&[
+            self.output_test1[0],
+            self.output_test2[0],
+            self.output_test3[0],
+        ]);
+        println!("Performing gradient descent");
+
+        for i in 0..truth.len() {
+            error[i] = (pred[i] - truth[i]) * (pred[i] - truth[i]);
+            delta[i] = pred[i] - truth[i];
+        }
+        let weight_deltas: Array2<f64> = Self::outer_prod(&inputs_vec, delta);
+
+        for i in 0..self.weights.nrows() {
+            for j in 0..self.weights.ncols() {
+                self.weights[[i, j]] -= self.alpha * weight_deltas[[i, j]];
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ fn main() {
     let number_of_toes: Array1<f64> = array![8.5, 9.5, 10.0, 9.0];
     let wlrec: Array1<f64> = array![0.65, 0.8, 0.8, 0.9];
     let nfans: Array1<f64> = array![1.2, 1.3, 0.5, 1.0];
+    let hurt: Array1<f64> = array![0.1, 0.0, 0.0, 0.1];
+    let win: Array1<f64> = array![1.0, 1.0, 0.0, 1.0];
+    let sad: Array1<f64> = array![0.1, 0.0, 0.1, 0.2];
 
     let weights: Array2<f64> =
         Array2::from_shape_vec((3, 3), vec![0.1, 0.1, -0.3, 0.1, 0.2, 0.0, 0.0, 1.3, 0.1]).unwrap();
-    let hidden_weights: Array2<f64> =
-        Array2::from_shape_vec((3, 3), vec![0.3, 1.1, -0.3, 0.1, 0.2, 0.0, 0.0, 1.3, 0.1]).unwrap();
 
-    let network = Network::build(weights, hidden_weights, number_of_toes, wlrec, nfans);
+    let mut network = Network::build(weights, number_of_toes, wlrec, nfans, 0.01, hurt, win, sad);
     network.pred();
 }
